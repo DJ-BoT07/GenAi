8e057538913a8f91e73038e29fe1974a235305e7
DJ-BoT07 <djmangaonkar7@gmail.com>
2024-11-07 02:57:39 +0530

    icon

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function Home() {
   return (
     <main className="min-h-screen p-4 md:p-8">
-      <Leaderboard />
+      <CountdownTimer />
     </main>
   )
 }
